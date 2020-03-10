c6b63fd75a7bc898bc3a45ece6a37b3a114ab3c4
Chaos <justin.db83@gmail.com>
2020-03-10 17:17:18 -0600

    updated login form using vaadin instead, adding background image

--- src/views/auth-view.js
+++ src/views/auth-view.js
@@ -3,62 +3,58 @@ import { BaseView } from './base-view';
 
 import '@vaadin/vaadin-text-field/vaadin-email-field'
 import '@vaadin/vaadin-text-field/vaadin-password-field'
-import '@vaadin/vaadin-button'
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-login';
 
 class AuthView extends LitElement{
   constructor(){
-    super(); 
+    super();
   }
 
   render() {
     return html`
       <style>
-        .form-container{
-          margin-top: 10px;
-          width: 500px;
-          padding: 25px;
-          height: 200px;
-          background-color: white;
-          margin-bottom: 10px;
-        }
+        .background-container{
+        background-image: url("../img/bg.jpg");
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+       -o-background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        height:100vh;  /* responsive height */
+      }
+      .form-container{
+        display: flex;
+        justify-content: center;
+        padding: 25px;
+      }
       </style>
-      <div class="container">
-        
-        <div class="form-container"> 
-          <h3>Login</h3>
-          <vaadin-email-field label="Email" placeholder="Enter Email" name="email" clear-button-visible
-          error-message="Please enter a valid email adress">
-          </vaadin-email-field>
-          <vaadin-password-field label="Password" name="password" placeholder="Enter Password"
-          error-message="Please enter a valid email adress"
-          reveal-button-visible
-          ></vaadin-password-field>
-          <vaadin-button type="submit"
-            theme="default"
-          >Submit</vaadin-button>
-        </div>
-        <div class="form-container">
-          <h3>Register</h3>
-          <vaadin-email-field label="Email" placeholder="Enter Email" name="email" clear-button-visible
-          error-message="Please enter a valid email adress">
-          </vaadin-email-field>
-          <vaadin-password-field label="Password" name="password" placeholder="Enter Password"
-          error-message="Please enter a valid email adress"
-          reveal-button-visible
-          ></vaadin-password-field>
-          <vaadin-password-field label="Confirm Password" name="passwordConfirm" placeholder="Enter Password"
-          error-message="Please enter a valid email adress"
-          reveal-button-visible
-          ></vaadin-password-field>
-          <vaadin-button type="submit"
-            theme="default"
-          >Submit</vaadin-button>
-        </div>
-        <div>
+      <div class="background-container">
+          <div class="form-container">
+          <vaadin-login-form></vaadin-login-form>
+          </div>
+          <vaadin-dialog id="feedbackDialog">
+            <template>Login is being processed</template>
+          </vaadin-dialog>
+          <vaadin-dialog id="supportDialog">
+            <template>Please contact support</template>
+          </vaadin-dialog>
       </div>
     `;
+  }
+
+  random() {
+    console.log("Inside class --> Hello")
+    
   }
 
 }
 
 customElements.define('auth-view', AuthView);
+
+//window.addEventListener('load', () => {
+//  console.log("outside class -- > hello");
+//  document.querySelector('vaadin-login-form')
+//})

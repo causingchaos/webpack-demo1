fa1869aa826d7c152fe341a031f7c3949a870614
causingchaos <none>
2020-03-11 01:48:54 -0600

    playing with shadow dom

--- src/views/auth-view.js
+++ src/views/auth-view.js
@@ -7,8 +7,10 @@ import '@vaadin/vaadin-button';
 import '@vaadin/vaadin-login';
 
 class AuthView extends LitElement{
-  constructor(){
-    super();
+  static get properties() {
+    return {
+      value: { type: Number }
+    }
   }
 
   render() {
@@ -25,34 +27,43 @@ class AuthView extends LitElement{
         width: 100%;
         height:100vh;  /* responsive height */
       }
-      .form-container{
+      main{
         display: flex;
         justify-content: center;
         padding: 25px;
       }
       </style>
       <div class="background-container">
-          <div class="form-container">
-          <vaadin-login-form @load ="${this.consoleLog}"></vaadin-login-form>
-          </div>
+          <main class="form-container">
+          <vaadin-login-form id="login-form""
+          @login="${(e) => console.log(e.target)}"
+          @forgot-password="${(e) => this.forgotDialog(e)}"
+          ></vaadin-login-form>
+          </main>
           <vaadin-dialog id="feedbackDialog">
             <template>Login is being processed</template>
           </vaadin-dialog>
-          <vaadin-dialog id="supportDialog">
+          <vaadin-dialog id="supportDialog"> 
             <template>Please contact support</template>
           </vaadin-dialog>
+
       </div>
     `;
   }
+  forgotDialog(e) {
+    var supportDialog= this.shadowRoot.querySelector('#supportDialog');
+    console.log(supportDialog);
+    supportDialog.opened = true;  // why does this not work!
+
+  }
   // firstUpdated lifecycle method fires after render
   firstUpdated() {
-    window.addEventListener('WebComponentsReady', function() {
-      var login = document.querySelector('vaadin-login-form');
-      var feedbackDialog = document.querySelector('#feedbackDialog');
-    })
-  }
+  };
 
-}
+  // event functions
+  
+
+};
 
 customElements.define('auth-view', AuthView);
 
@@ -60,3 +71,23 @@ customElements.define('auth-view', AuthView);
 //  console.log("outside class -- > hello");
 //  document.querySelector('vaadin-login-form')
 //})
+/*
+window.addEventListener('WebComponentsReady', function() {
+  var login = shadowRoot.getElementById('login-form');
+  var feedbackDialog = document.querySelector('#feedbackDialog');
+  var supportDialog = document.querySelector('#supportDialog');
+
+  login.addEventListener('login', function() {
+    feedbackDialog.opened = true;
+
+    setTimeout(function() {
+      login.disabled = false;
+      feedbackDialog.opened = false;
+    }, 2000)
+  });
+
+  login.addEventListener('forgot-password', function() {
+    supportDialog.opened = true;
+  });
+});
+*/
